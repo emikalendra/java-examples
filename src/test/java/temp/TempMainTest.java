f718fa7d199efb42e668a94c9eedbfd12b8455dc
Emi Kalendra <emi.kalendra@gmail.com>
2013-09-25 21:37:41 -0700

    added basic files to directory stucture to hold places

---
+++ src/test/java/temp/TempMainTest.java
@@ -0,0 +1,21 @@
+/**
+ * Very basic test to act as a place holder
+ */
+package temp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Emi Kalendra
+ *
+ */
+public class TempMainTest {
+
+	@Test
+	public void testTest(){
+		assertTrue(true);
+		//fail();
+	}
+}
