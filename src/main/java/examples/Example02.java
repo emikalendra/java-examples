b3c545a9f8327d270614db4ff23c01eb7e5bb8dc
Emi Kalendra <emi.kalendra@gmail.com>
2014-05-19 23:15:38 -0700

    some work with example 2

--- src/main/java/examples/Example02.java
+++ src/main/java/examples/Example02.java
@@ -4,7 +4,27 @@ package examples;
  * Created by emikalendra on 5/19/14.
  */
 public class Example02 {
-    public static void main (String[] args){
-        
+    public static void main(String[] args) {
+        Cat myCat = new Cat("reko", "white");
+        myCat.sayHi();
+        System.out.println("get name by getter: " + myCat.getName());
+    }
+}
+
+class Cat {
+    private String name;
+    private String color;
+
+    public Cat(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void sayHi() {
+        System.out.println("I am a Cat. Hi I am " + getName() + " and my color is " + color);
     }
 }
